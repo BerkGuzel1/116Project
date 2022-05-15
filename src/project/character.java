9cddbad5c8bc721468af3493ddbaaac214fed9c3
BerkGuzel1 <berkgguzel@gmail.com>
2022-05-15 22:18:58 +0300

    first version

--- src/project/character.java
+++ src/project/character.java
@@ -4,6 +4,8 @@ public class character {
     public static void main(String[] args) {
 
         int x = 5;
+        int y=4;
         System.out.println(x);
+        System.out.println(y);
     }
  }
