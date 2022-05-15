72d1ee265b5553ab905fad20c2150e75a7fdd348
BerkGuzel1 <berkgguzel@gmail.com>
2022-05-15 22:12:44 +0300

    first version

--- src/project/character.java
+++ src/project/character.java
@@ -1,4 +1,9 @@
 package project;
 
 public class character {
-}
+    public static void main(String[] args) {
+
+        int x = 5;
+        System.out.println(x);
+    }
+ }
--- src/project/player.java
+++ src/project/player.java
@@ -1,7 +1,10 @@
 package project;
 
+import java.util.Scanner;
+
 public class player {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
     }
 }
