363e2632379d3f7a830a41905d858c38a2d5ace6
Burak Şafak <burakshafak@hotmail.com>
2022-05-22 20:01:11 +0300

    a

--- src/project/Lsword.java
+++ src/project/Lsword.java
@@ -3,8 +3,6 @@ package project;
 public class Lsword extends Weapon {
     public Lsword(String name, int weight, int value, int damage) {
         super(name, weight, value, damage);
-
-        Character character;
         name = "Long sword";
         weight = 10;
         value = 6;
--- src/project/Ssword.java
+++ src/project/Ssword.java
@@ -4,11 +4,9 @@ public class Ssword extends Weapon {
 
     public Ssword(String name, int weight, int value, int damage) {
         super(name, weight, value, damage);
-
-        Character character;
         name = "Short sword";
         weight = 5;
         value = 2;
-        //damage = 10 * character.getMaxStrength();
+        damage = 10 ;
     }
 }
