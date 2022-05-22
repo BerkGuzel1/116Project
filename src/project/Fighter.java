85ac4edd2a5ac175d6871ffc03fee37b6b789a8a
BerkGuzel1 <berkgguzel@gmail.com>
2022-05-22 21:56:05 +0300

    first version

--- src/project/Fighter.java
+++ src/project/Fighter.java
@@ -4,13 +4,6 @@ import java.util.Random;
 
 public class Fighter extends Character {
 
-    /*public Fighter(int maxStrength, int maxVitality, int maxIntelligence, int maxHP) {
-        super(generateValue6to10(), generateValue3to7(), generateValue1to5(), generateValue1to5());
-        float maxHp = (float) (getMaxIntelligence()*0.1 + getMaxVitality()*0.7 + getMaxStrength()*0.2);
-        System.out.println("Fighter generated. Strength:" + getMaxStrength()+ " Vitality:"+ getMaxVitality() + " Intelligence:" + getMaxIntelligence() + " HP:" + maxHp);
-
-    }*/
-
 
     public Fighter(int maxStrength, int maxVitality, int maxIntelligence, int maxHP) {
         super(generateRandomValue(10,6), generateRandomValue(7,3), generateRandomValue(5,1), generateRandomValue(5,1));
@@ -24,27 +17,6 @@ public class Fighter extends Character {
         int value = rand.nextInt(upperBound-lowerBound+1) + lowerBound;
         return value;
     }
-
-    /*private static int generateValue6to10(){
-        Random rand = new Random();
-        int upperBound = 3;
-        int value = rand.nextInt(upperBound) + 6;
-        return value;
-    }
-
-    private static int generateValue3to7(){
-        Random rand = new Random();
-        int upperBound = 4;
-        int value = rand.nextInt(upperBound) + 3;
-        return value;
-    }
-
-    private static int generateValue1to5(){
-        Random rand = new Random();
-        int upperBound = 6;
-        int value = rand.nextInt(upperBound) ;
-        return value;
-    }*/
 
     @Override
     public void wield() {
--- src/project/Tank.java
+++ src/project/Tank.java
@@ -10,57 +10,10 @@ public class Tank extends Character {
     }
 
 
-
-        public static int generateRandomValue(int upperBound, int lowerBound){
+    public static int generateRandomValue(int upperBound, int lowerBound) {
         Random rand = new Random();
-        int value = rand.nextInt(upperBound-lowerBound+1) + lowerBound;
+        int value = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
         return value;
     }
-
-
-
-    /*public Tank() {
-        setMaxStrength(generateRandomValue(5,1));
-        setMaxVitality(generateRandomValue(10,6));
-        setMaxIntelligence(generateRandomValue(7,3));
-        System.out.println("A tank is generated with the values: S:" + getMaxStrength() + " V:"+ getVitality() + " I:" + getIntelligence());
-
-    }*/
-
-    /*public Tank(int maxStrength, int maxVitality, int maxIntelligence, int maxHP) {
-        super(generateRandomValue(5,1), generateRandomValue(10,6), generateRandomValue(7,3), generateRandomValue(5,1));
-        float maxHp = (float) (getMaxIntelligence()*0.1 + getMaxVitality()*0.7 + getMaxStrength()*0.2);
-        System.out.println("Tank generated. Strength:" + getMaxStrength()+ " Vitality:"+ getMaxVitality() + " Intelligence:" + getMaxIntelligence() + " HP:" + maxHp);
-
-    }
-
-    public static int generateRandomValue(int upperBound, int lowerBound){
-        Random rand = new Random();
-        int value = rand.nextInt(upperBound-lowerBound+1) + lowerBound;
-        return value;
-    }*/
-
-
-    //double hp =  ((getMaxIntelligence() * 0.1) + (getMaxStrength() * 0.2) + (getMaxVitality() * 0.7));
-
-
-
-
-
-
-
-    /*public int generateVitalityValue(){
-        Random rand = new Random();
-        int upperBound = 6;
-        int value = rand.nextInt(upperBound);
-        return value;
-    }*/
-
-    /*public int generateIntelliginceValue(){
-        Random rand = new Random();
-        int upperBound = 6;
-        int value = rand.nextInt(upperBound);
-        return value;
-    }*/
 
 }
