a7c4fa7eb1986a9c622cff282c7135843aa81474
niranu <niranugurer@yahoo.com>
2022-05-22 22:13:47 +0300

    e

--- src/project/Game.java
+++ src/project/Game.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -9,5 +11,76 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your name : ");
         String playerName =sc.next();
+    }
+
+    protected Fighter fighter;
+    protected Healer healer;
+    protected Tank tank;
+    protected Enemy enemy;
+    protected ArrayList<Level> levels;
+    protected Level currentLevel;
+
+
+    public boolean isOver(){
+        if(fighter.getHP() == 0 && healer.getHP()==0 && tank.getHP()==0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static int generateRandomValue(int upperBound, int lowerBound){
+        Random rand = new Random();
+        int value = rand.nextInt(upperBound-lowerBound+1) + lowerBound;
+        return value;
+    }
+
+
+
+    public void startGame(){
+        System.out.println("Welcome to Cannon Fodder!");
+        System.out.println("--------------------------------");
+        currentLevel = new Level();
+        currentLevel.levelID++;
+        generateTank();
+        generateFighter();
+        generateHealer();
+        generateEnemy();
+
+    }
+
+    public void runGame(){
+
+    }
+
+
+
+
+    public void generateTank(){
+        tank = new Tank(generateRandomValue(5,1),
+                generateRandomValue(10,6),
+                generateRandomValue(7,3),
+                generateRandomValue(5,1));
+    }
+
+    public void generateFighter(){
+        fighter = new Fighter(generateRandomValue(10,6),
+                generateRandomValue(7,3),
+                generateRandomValue(5,1),
+                generateRandomValue(5,1));
+    }
+
+    public void generateHealer(){
+        healer = new Healer(generateRandomValue(7,3),
+                generateRandomValue(5,1),
+                generateRandomValue(10,6),
+                generateRandomValue(5,1));
+    }
+
+    public void generateEnemy(){
+        enemy = new Enemy(generateRandomValue(5,1),
+                generateRandomValue(5,1),
+                generateRandomValue(5,1),
+                generateRandomValue(5,1));
     }
 }
--- src/project/Level.java
+++ src/project/Level.java
@@ -1,14 +1,43 @@
 package project;
 
 public class Level {
-    int levelCounter = -1;
-    int enemyNumber;
+    protected static int levelNum = 0;
+    protected int levelID =-1;
+    protected Fighter fighter;
+    protected Tank tank;
+    protected Healer healer;
+    protected Enemy enemy;
+    //3 levelde bir rastgele silah olsunint levelCounter = -1;
+    int enemyNumber= (int) Math.pow(2,levelID);
 
-     public void start(){
+    public Level() {
+        System.out.println("A new level is being generated...");
+        levelID++;
+        levelNum++;
+        System.out.println("Current Level ID:" + levelID);
+        System.out.println("There are " + Math.pow(2,levelID) + " enemies in this level.");
+        if (levelID % 3 == 2){
+            System.out.println("There is a weapon or armor in this level that you can obtain after defeating all enemies.");
+            //3 levelde bir silah olsun
+        }
+        System.out.println("--------------------------------");
+    }
+
+
+    public void start(){
          levelCounter++;
          enemyNumber = (int) Math.pow(2,levelCounter);
          System.out.println("Creating Level " + levelCounter + " with " + enemyNumber + " enemy soldier.");
          System.out.println("Entering Level " + levelCounter + ";" );
      }
+
+
+
+
+
+    /*public void start(){
+
+
+    }*/
 
 }
