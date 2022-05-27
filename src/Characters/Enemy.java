1200b89ad01fd782780d5f90dbd8507a80f77528
niranu <niranugurer@yahoo.com>
2022-05-27 15:55:04 +0300

    enemy weapon

--- src/Characters/Enemy.java
+++ src/Characters/Enemy.java
@@ -1,6 +1,9 @@
 package Characters;
 
 import Characters.Character;
+import Items.Shield;
+import Items.Sword;
+import Items.Wand;
 
 import java.util.Random;
 
@@ -13,7 +16,24 @@ public class Enemy extends Character {
         return value;
     }
 
+    public static int generateValue1to10(){
+        Random rand = new Random();
+        int upperBound = 10;
+        int value = rand.nextInt(upperBound) +1 ;
+        return value;
+    }
+
     public Enemy(int maxStrength, int maxVitality, int maxIntelligence, int maxHP) {
         super(generateValue1to5(), generateValue1to5(), generateValue1to5(), generateValue1to5());
+        int rnd = generateValue1to10();
+        if (rnd >=1 && rnd<9){
+            this.setWeapon(new Sword("Broken Sword",4,1,3));
+        }
+        else if(rnd== 9){
+            this.setWeapon(new Wand("Broken Wand", 2,1,2));
+        }
+        else if(rnd == 10){
+            this.setWeapon(new Shield("Broken Shield", 3,1,1));
+        }
     }
 }
